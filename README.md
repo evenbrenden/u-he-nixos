190a1959931a417db845c66a4a172bc7bc36c9c0
Even Brenden <evenbrenden@gmail.com>
2020-11-29 16:22:28 +0100

    Use URL instead of local file

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 Until I can figure out how to do this purely:
 
-* Build the package (example plugin):
-    * `$ nix-build -E '((import <nixpkgs> {}).callPackage (import ./u-he-diva.nix) { })'`
+* Build a package (for example `./u-he-diva.nix`):
+    * `$ nix-build -E '((import <nixpkgs> {}).callPackage (import [package]) {})'`
 * Make a writable copy of the resulting build:
     * `$ cp -r result/ build`
     * `$ chmod -R +w build`
--- u-he-diva.nix
+++ u-he-diva.nix
@@ -1,8 +1,11 @@
-{ stdenv, autoPatchelfHook, expat, libuuid, glib, gtk3 }:
+{ pkgs, stdenv, autoPatchelfHook, expat, libuuid, glib, gtk3 }:
 
 stdenv.mkDerivation rec {
   name = "u-he-diva";
-  src = ./Diva_144_9775_Linux.tar.xz;
+  src = pkgs.fetchurl {
+    url = "https://www.u-he.com/downloads/linux/beta/9775/Diva_144_9775_Linux.tar.xz";
+    sha256 = "0dz5mrnxqvj28kbhsvjnq341l8k5zhh3ncw13pzpk8wcffqk16qs";
+  };
 
   nativeBuildInputs = [ autoPatchelfHook ];
   buildInputs = [ expat libuuid glib gtk3 ];

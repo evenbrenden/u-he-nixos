2e74e0aecac4e02fbf32b3881ad0a0ba469ab678
Even Brenden <evenbrenden@users.noreply.github.com>
2023-06-18 10:35:48 +0200

    Add Uhbik

---
+++ u-he-uhbik.nix
@@ -0,0 +1,8 @@
+with import <nixpkgs> {};
+
+let
+  name = "u-he-diva";
+  url = "https://dl.u-he.com/releases/Uhbik_131_8256_Linux.tar.xz";
+  sha256 = "sha256-kYL7pOGIJv1++p+PMNOy/g6L8dOQ/45yc+KgqpNSyrM=";
+in
+  pkgs.callPackage (import ./common.nix) { pluginName = name; pluginUrl = url; pluginSha256 = sha256; }

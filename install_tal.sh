0437fdf3b6735af80b355d1a1c2bd8402fcb2329
wsz <o9b9qzaa@wsz.anonaddy.com>
2023-08-15 21:23:25 +0200

    automatic push

--- install_tal.sh
+++ install_tal.sh
@@ -3,3 +3,4 @@
 export NIXPKGS_ALLOW_UNFREE=1
 nix-build -E 'with import <nixpkgs> {}; callPackage ./tal-bassline-101.nix {}'
 ln -sf $(realpath result)/lib/tal-bassline-101 ~/.vst/TAL-BassLine-101
+rm -r ./result

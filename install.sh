0553ab47b8812b65fd1f1e904bb3cee0b0cf6720
Even Brenden <evenbrenden@users.noreply.github.com>
2026-05-14 20:35:34 +0200

    install.sh: Always clean up
    Make sure hidden files are copied.
    
    Use consistent quoting.

--- install.sh
+++ install.sh
@@ -3,21 +3,21 @@
 
 set -e
 
-if [[ $# -ne 1 ]]; then
+trap "{ rm -rf result; rm -rf build; }" EXIT
+
+if [[ "$#" -ne 1 ]]; then
     echo "Usage: install.sh [package file]"
     exit 1
 fi
 
-PACKAGE=$1
+PACKAGE="$1"
 
-if [[ ! -f $PACKAGE ]]; then
+if [[ ! -f "$PACKAGE" ]]; then
     echo "$PACKAGE not found"
     exit 1
 fi
 
 nix-build "$PACKAGE"
-cp -r result/ build
+cp -r result/. build
 chmod -R +w build
 ./build/lib/install.sh
-rm -r result
-rm -r build

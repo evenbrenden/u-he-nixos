f2bea4c3566d69ae9ef59f1ae1a9b3e4f50c13d6
Even Brenden <evenbrenden@users.noreply.github.com>
2026-05-14 21:41:46 +0200

    install.sh: Tidy up

--- install.sh
+++ install.sh
@@ -1,16 +1,20 @@
 #! /usr/bin/env nix-shell
 #! nix-shell -i bash -p bintools
 
-set -e
+set -euo pipefail
 
-trap "{ rm -rf result; rm -rf build; }" EXIT
+trap "rm -rf result build" EXIT
 
-if [[ "$#" -ne 1 ]]; then
-    echo "Usage: install.sh [flake output/plugin name]"
+if [[ $# -ne 1 ]]; then
+    echo "Usage: $0 [flake output/plugin name]"
     exit 1
 fi
 
-nix build ".#$1"
+flake_output="$1"
+
+nix build ".#${flake_output}"
+rm -rf build
 cp -r result/. build
 chmod -R +w build
+
 ./build/lib/install.sh

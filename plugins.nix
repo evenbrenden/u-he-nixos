1b028439d6c8f05d927f9b630d72ef16f9d02613
Merleur l'enchantin <le.neko@gmail.com>
2026-07-06 23:06:47 +0200

    add: missing unfree plugins

--- plugins.nix
+++ plugins.nix
@@ -10,10 +10,47 @@
     url = "https://dl.u-he.com/releases/Diva_148_16519_Linux.tar.xz";
     hash = "sha256-gOYklSY7TIsLFRWRXlnnNvsxhgAH4zBgP48ftWuNqvM=";
   };
+  hive2 = {
+    url = "https://dl.u-he.com/releases/Hive_212_16520_Linux.tar.xz";
+    hash = "sha256-8rUvi4w20HTSULLtnlYX/dDYJYh5HhMU/KEV6/xLyUU=";
+  };
+  repro = {
+    url = "https://dl.u-he.com/releases/Repro_113_16797_Linux.tar.xz";
+    hash = "sha256-MYIvdOdzdcGFNQEPu2mAF50ROYUsO9ji1JXmLwDBtuM=";
+  };
+  bazille = {
+    url = "https://dl.u-he.com/releases/Bazille_113_16518_Linux.tar.xz";
+    hash = "sha256-siHzcIg/KlGB5MmzqMNmuro4PNQMGUhHn9t1XUJdiFs=";
+  };
+  ace = {
+    url = "https://dl.u-he.com/releases/ACE_143_16518_Linux.tar.xz";
+    hash = "sha256-DhV7TNLNlrezkjc3r4+3vNYeSMZ6GBLGAUdJ1j2DZnc=";
+  };
+
   ## Effects
   satin = {
     url = "https://dl.u-he.com/releases/Satin_133_15721_Linux.tar.xz";
     hash = "sha256-FmjgH+kt6E6bWLrGvUzABwhU7CdZ6AitFiZRjJY+M18=";
+  };
+  presswerk = {
+    url = "https://dl.u-he.com/releases/Presswerk_116_16960_Linux.tar.xz";
+    hash = "sha256-KAokFzP0y9iwUmH80Lwb2hcdfI+Nzu6LVr1ifXUtSnI=";
+  };
+  colourcopy = {
+    url = "https://dl.u-he.com/releases/ColourCopy_102_16742_Linux.tar.xz";
+    hash = "sha256-R9LTuNhOsX2ss2GtMhtgaGZ+OtxwdXfX5uZxmgCVCrc=";
+  };
+  filterscape = {
+    url = "https://dl.u-he.com/releases/Filterscape_151_15664_Linux.tar.xz";
+    hash = "sha256-ddme6VFRpJUMtGsIm3FzkcX8cjQdG8Z1qDG255R4VvM=";
+  };
+  mfm2 = {
+    url = "https://dl.u-he.com/releases/MFM2_251_16742_Linux.tar.xz";
+    hash = "sha256-x7o533MYLYy0YjgkDist0o0IoN3Q8Uf+L0Dl4EoT3bc=";
+  };
+  twangstrom = {
+    url = "https://dl.u-he.com/releases/Twangstrom_102_16742_Linux.tar.xz";
+    hash = "sha256-DXOMpIsC+EV3TGlRrWJpHTLQ/lcOUHs44iTXrV/1M6o=";
   };
   uhbik2 = {
     url = "https://u-he.com/downloads/releases/Uhbik_200_19684_Linux.tar.xz";

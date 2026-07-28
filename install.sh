f39dfa8d020b4a6d5fbe42febf36aad0fade9172
Even Brenden <evenbrenden@users.noreply.github.com>
2026-07-28 20:49:07 +0200

    install.sh: Improve help text

--- install.sh
+++ install.sh
@@ -6,7 +6,9 @@ set -euo pipefail
 trap "rm -rf result build" EXIT
 
 if [[ $# -ne 1 ]]; then
-    echo "Usage: $0 [flake output/plugin name]"
+    echo "Usage: $0 [plugin]"
+    echo
+    echo "Available plugins:"
     nix eval --raw .#packages.x86_64-linux --apply 'pluginName: builtins.concatStringsSep "\n" (builtins.attrNames pluginName)'
     exit 1
 fi

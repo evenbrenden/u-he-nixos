11ef73ebca078ef922c77ce06397d65657271187
Even Brenden <evenbrenden@users.noreply.github.com>
2026-07-28 14:48:34 +0200

    install.sh: Change casing in Nix expression
    For consistency with flake.nix.

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ trap "rm -rf result build" EXIT
 
 if [[ $# -ne 1 ]]; then
     echo "Usage: $0 [flake output/plugin name]"
-    nix eval --raw .#packages.x86_64-linux --apply 'plugin_name: builtins.concatStringsSep "\n" (builtins.attrNames plugin_name)'
+    nix eval --raw .#packages.x86_64-linux --apply 'pluginName: builtins.concatStringsSep "\n" (builtins.attrNames pluginName)'
     exit 1
 fi
 
